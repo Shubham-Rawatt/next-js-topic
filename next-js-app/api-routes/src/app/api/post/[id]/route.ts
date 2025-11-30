566fc99c7fe6da7ce66f8c843a463b08698be96b
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-11-30 13:31:01 +0530

    practice methods

--- next-js-app/api-routes/src/app/api/post/[id]/route.ts
+++ next-js-app/api-routes/src/app/api/post/[id]/route.ts
@@ -12,3 +12,32 @@ export async function GET(req: NextRequest, { params }: paramsType) {
     postId: id,
   });
 }
+
+
+
+/*
+ -->     practice code api methods
+
+
+     export async function GET() {
+     
+        return NextResponse.json({
+            id: 1,
+            name:'api',
+            version : 16
+        })
+     }
+
+
+
+     export async funtion POST(req: NextRequest){
+     let {id , name , version} = await req.json()
+        return NextResponse.json({
+            id, name, version
+     })
+     
+     }
+
+
+
+*/
