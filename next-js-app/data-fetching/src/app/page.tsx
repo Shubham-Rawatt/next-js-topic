571edc6e3a1ba8ef6a493243894d31caefa861e1
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-12-02 19:09:01 +0530

    practice SSR & SSG side rendring in next

--- next-js-app/data-fetching/src/app/page.tsx
+++ next-js-app/data-fetching/src/app/page.tsx
@@ -2,9 +2,17 @@ import { log } from 'console'
 import React from 'react'
 
 const Home = async () => {
-  let res = await fetch('http://localhost:3000/api/user',{cache:'no-store'})
-  let data = res.json()
-console.log(data)
+  //-- SSR   (server side rendring)
+//   let res = await fetch('http://localhost:3000/api/user',{cache:'no-store'})
+//   let data = await res.json()
+// console.log(data)
+
+
+// -- SSG   (static side genration)
+  let res = await fetch('http://localhost:3000/api/user',{cache:'force-cache'})
+  let data = await res.json()
+  console.log(data)
+
 
 
   return (
